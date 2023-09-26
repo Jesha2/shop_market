2491422b60b5b7de81d47464385fd6106128eb36
fajesha nasim <fajesha1@gmail.com>
2023-09-26 12:32:11 -0500

    checkout enabled

--- src/Components/Checkout.js
+++ src/Components/Checkout.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
+import { useCartValue } from "../store/cartContext";
 
 import CheckoutForm from "./CheckoutForm";
 
@@ -12,17 +13,19 @@ const stripePromise = loadStripe("pk_test_51NuKAmLB8ww5YxOuEUpQIdhKhtLOY3w0JWVuV
 
 const  Checkout=() =>{
   const [clientSecret, setClientSecret] = useState("");
-
-  useEffect(() => {
-    // Create PaymentIntent as soon as the page loads
-    fetch("/create-payment-intent", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ items: [{ id: "xl-tshirt" }] }),
-    })
-      .then((res) => res.json())
-      .then((data) => setClientSecret(data.clientSecret));
-  }, []);
+  const [{ cart }] = useCartValue();
+ 	useEffect(() => {
+	const cartItemIds = cart.map((item) => item.id);
+		//const subtotal = getCartTotal(cart);
+		// Create PaymentIntent as soon as the page loads
+		fetch("/create-payment-intent", {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({ items: cartItemIds }),
+		})
+			.then((res) => res.json())
+			.then((data) => setClientSecret(data.clientSecret));
+	}, [cart]);
 
   const appearance = {
     theme: 'stripe',
