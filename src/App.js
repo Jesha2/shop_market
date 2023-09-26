4b2db92a86c18f4dd5a4fc61e16ca70ddf2e14dc
fajesha nasim <fajesha1@gmail.com>
2023-09-26 12:27:53 -0500

    added stripe

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Header from "./components/Header";
 import Login from "./components/Login";
 import Cart from "./components/Cart";
 import Home from "./components/Home";
-import CheckoutForm from "./components/CheckoutForm";
+import Checkout from "./components/Checkout";
 import Product from "./components/Product";
 import PaymentComplete from "./components/PaymentComplete"
 import Footer from "./components/Footer";
@@ -17,40 +17,38 @@ import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 // import { CartProvider } from "./store/cartContext";
 import Auth from "./components/Auth";
 
-import { loadStripe } from "@stripe/stripe-js";
-import { Elements } from "@stripe/react-stripe-js";
+////import { loadStripe } from "@stripe/stripe-js";
+//import { Elements } from "@stripe/react-stripe-js";
 
-const stripePromise = loadStripe(
-	"pk_test_51NuKAmLB8ww5YxOuEUpQIdhKhtLOY3w0JWVuVjLnTBdlHbALgtmVL9fn7E8ZYu6g4UxqKK6Sdl86PmGpsErNoi9W00IffIhwuu"
-);
+
 
 function App() {
 	const { state } = useContext(AuthContext);
 	const [{ cart }] = useCartValue();
 	
 
-	const [clientSecret, setClientSecret] = useState("");
+	// const [clientSecret, setClientSecret] = useState("");
 
-	useEffect(() => {
-	const cartItemIds = cart.map((item) => item.id);
-		//const subtotal = getCartTotal(cart);
-		// Create PaymentIntent as soon as the page loads
-		fetch("/create-payment-intent", {
-			method: "POST",
-			headers: { "Content-Type": "application/json" },
-			body: JSON.stringify({ items: cartItemIds }),
-		})
-			.then((res) => res.json())
-			.then((data) => setClientSecret(data.clientSecret));
-	}, [cart]);
+	// useEffect(() => {
+	// const cartItemIds = cart.map((item) => item.id);
+	// 	//const subtotal = getCartTotal(cart);
+	// 	// Create PaymentIntent as soon as the page loads
+	// 	fetch("/create-payment-intent", {
+	// 		method: "POST",
+	// 		headers: { "Content-Type": "application/json" },
+	// 		body: JSON.stringify({ items: cartItemIds }),
+	// 	})
+	// 		.then((res) => res.json())
+	// 		.then((data) => setClientSecret(data.clientSecret));
+	// }, [cart]);
 
-	const appearance = {
-		theme: "stripe",
-	};
-	const options = {
-		clientSecret,
-		appearance,
-	};
+	// const appearance = {
+	// 	theme: "stripe",
+	// };
+	// const options = {
+	// 	clientSecret,
+	// 	appearance,
+	// };
 
 	return (
 		<div className="App">
@@ -59,7 +57,7 @@ function App() {
 					<Header />
 					<Routes>
 						<Route path="/auth" element={<Auth />} />
-						{clientSecret && (
+						{/* {clientSecret && (
 							<Route
 								path="/checkout"
 								element={
@@ -72,8 +70,8 @@ function App() {
 									)
 								}
 							/>
-						)}
-						{/* <Route path='/checkout' element={state.token ? <CheckoutForm/> : <Navigate to="/auth" />}/> */}
+						)} */}
+						<Route path='/checkout' element={state.token ? <Checkout/> : <Navigate to="/auth" />}/>
 
 						<Route path="/cart" element={<Cart />} />
 						<Route path="/paymentComplete" element={<PaymentComplete />} />
---
+++ src/Components/Checkout.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+import CheckoutForm from "./CheckoutForm";
+
+
+// Make sure to call loadStripe outside of a component’s render to avoid
+// recreating the Stripe object on every render.
+// This is your test publishable API key.
+const stripePromise = loadStripe("pk_test_51NuKAmLB8ww5YxOuEUpQIdhKhtLOY3w0JWVuVjLnTBdlHbALgtmVL9fn7E8ZYu6g4UxqKK6Sdl86PmGpsErNoi9W00IffIhwuu");
+
+const  Checkout=() =>{
+  const [clientSecret, setClientSecret] = useState("");
+
+  useEffect(() => {
+    // Create PaymentIntent as soon as the page loads
+    fetch("/create-payment-intent", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ items: [{ id: "xl-tshirt" }] }),
+    })
+      .then((res) => res.json())
+      .then((data) => setClientSecret(data.clientSecret));
+  }, []);
+
+  const appearance = {
+    theme: 'stripe',
+  };
+  const options = {
+    clientSecret,
+    appearance,
+  };
+
+  return (
+    <div className="App">
+      {clientSecret && (
+        <Elements options={options} stripe={stripePromise}>
+          <CheckoutForm />
+        </Elements>
+      )}
+    </div>
+  );
+}
+
+export default Checkout;
